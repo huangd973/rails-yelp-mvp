bd781c87a86790cf6aa462f2924e36162614e95e
huang <huangdey973@gmail.com>
2017-11-22 15:07:21 +0100

    exo1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+      # t.string :name
+      # t.string :address
+      # t.string :phone_number
+      # t.string :category
+      # t.string :reviews
+
+Restaurant.destroy_all
+
+5.times do
+  art = Restaurant.new
+  art.name = "Petit poisson"
+  art.address = "danger danger"
+  art.phone_number = "0640394885"
+  art.category = "chinese"
+  art.save
+end
